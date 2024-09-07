2c6316b03bea40c0b002f78d64fbdb338b76241d
Arvind-Atri <148221394+Arvind-Atri@users.noreply.github.com>
2024-09-07 11:44:04 +0530

    Update Navbar.jsx

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -9,6 +9,7 @@ import { categories } from "../../services/apis";
 import { IoIosArrowDropdownCircle } from "react-icons/io";
 import ProfileDropDown from "../core/Auth/ProfileDropDown";
 import { ACCOUNT_TYPE } from "../../utils/constants";
+import { BsChevronDown } from "react-icons/bs";
 import { AiOutlineShoppingCart, AiOutlineMenu } from "react-icons/ai";
 import NavbarModal from "./NavbarModal";
 import { RxCross1 } from "react-icons/rx";
@@ -19,6 +20,7 @@ const Navbar = () => {
   const location = useLocation();
 
   const [subLinks, setSubLinks] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [display, setDisplay] = useState(false);
 
   const matchRoutes = (route) => {
@@ -37,54 +39,88 @@ const Navbar = () => {
   }, []);
   return (
     <>
-      <div className="border-b-2 bg-richblack-800 border-richblack-700 h-14 flex items-center ">
-        <div className="w-11/12 max-w-maxContent mx-auto flex justify-between items-center ">
-          <Link to={"/"}>
-            <img src={logo} alt="logo" loading="lazy" width={160} height={42} />
+      <div
+        className={`flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700 ${
+          location.pathname !== "/" ? "bg-richblack-800" : ""
+        } transition-all duration-200`}
+      >
+        <div className="flex w-[96%] md:w-11/12 max-w-maxContent items-center justify-between">
+          {/* Logo */}
+          <Link to="/">
+            <img
+              src={logo}
+              alt="Logo"
+              width={160}
+              height={32}
+              loading="lazy"
+              className="rounded-xl p-2 d "
+            />
           </Link>
-          <ul className=" relative  text-white gap-4 hidden md:flex ">
-            {NavbarLinks.map((element, index) => (
-              <li key={index}>
-                {element.title === "Catalog" ? (
-                  <div className="flex relative  cursor-pointer items-center gap-x-1 group">
-                    <p>{element.title}</p>
-                    <IoIosArrowDropdownCircle />
-                    <div className="invisible z-[1000]   opacity-0 group-hover:visible group-hover:opacity-100 absolute p-4 top-[153%] translate-x-[-50%] left-[60%] flex flex-col transition-all duration-200 bg-richblack-5 text-richblack-900 rounded-md lg:w-[300px]">
-                      <div className="absolute  h-6 w-6 left-[54%] -top-3 rotate-45 bg-richblack-5"></div>
-                      {subLinks.length ? (
-                        subLinks.map((subLink, index) => (
-                          <Link
-                            className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
-                            to={`/catalog/${subLink.name
-                              .split(" ")
-                              .join("-")
-                              .toLowerCase()}`}
-                            key={index}
-                          >
-                            <p>{subLink.name}</p>
-                          </Link>
-                        ))
-                      ) : (
-                        <div></div>
-                      )}
-                    </div>
-                  </div>
-                ) : (
-                  <Link to={element?.path}>
-                    <p
-                      className={`${
-                        matchRoutes(element?.path)
-                          ? "text-yellow-200"
-                          : "text-white"
-                      }`}
-                    >
-                      {element.title}
-                    </p>
-                  </Link>
-                )}
-              </li>
-            ))}
-          </ul>
+          {/* Navigation links */}
+
+          <nav className="">
+            <ul className="flex md:gap-x-6 text-richblack-25">
+              {NavbarLinks.map((link, index) => (
+                <li key={index}>
+                  {link.title === "Catalog" ? (
+                    <>
+                      <div
+                        className={`group relative flex cursor-pointer items-center sm:-ml-6 md:ml-1  gap-1 ${
+                          matchRoutes("/catalog/:catalogName")
+                            ? "text-yellow-25"
+                            : "text-richblack-25"
+                        }`}
+                      >
+                        <p>{link.title}</p>
+                        <BsChevronDown />
+                        <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
+                          <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
+                          {loading ? (
+                            <p className="text-center">Loading...</p>
+                          ) : (
+                            <>
+                              {subLinks && subLinks.length > 0 ? (
+                                subLinks
+                                  .filter(
+                                    (subLink) => subLink?.courses?.length > 0
+                                  )
+                                  .map((subLink, i) => (
+                                    <Link
+                                      to={`/catalog/${subLink.name
+                                        .split(" ")
+                                        .join("-")
+                                        .toLowerCase()}`}
+                                      className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
+                                      key={i}
+                                    >
+                                      <p>{subLink.name}</p>
+                                    </Link>
+                                  ))
+                              ) : (
+                                <p className="text-center">No Courses Found</p>
+                              )}
+                            </>
+                          )}
+                        </div>
+                      </div>
+                    </>
+                  ) : (
+                    <Link to={link?.path}>
+                      <p
+                        className={`${
+                          matchRoutes(link?.path)
+                            ? "text-yellow-25 hidden md:block"
+                            : "text-richblack-25 hidden md:block"
+                        }`}
+                      >
+                        {link.title}
+                      </p>
+                    </Link>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </nav>
           {/* login/signup/dashboard */}
           <div className=" gap-x-4 items-center hidden md:flex">
             {user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
